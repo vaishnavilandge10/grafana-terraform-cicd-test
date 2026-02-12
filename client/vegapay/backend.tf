633b436e1ae8268257f8c238763237ac1ced7a29
Vaishnavi <vaishnavi.landge@vegapay.tech>
2026-02-12 11:59:19 +0530

    Update backend state path for prod

--- client/vegapay/backend.tf
+++ client/vegapay/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "grafana-terraform-state-vegapay"
-    key            = "vegapay/grafana/terraform.tfstate"
+    key            = "vegapay/prod/grafana/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-lock"
     encrypt        = true
